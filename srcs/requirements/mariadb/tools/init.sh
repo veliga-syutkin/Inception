236d4b6587b8932fe9de5739219a2aa45531ba7f
Veliga Syutkin <veliga99@gmail.com>
2025-09-30 16:51:12 +0200

    shenaningans

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 service mysql start
+echo "Initializing database..."
 sleep 5
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
@@ -10,4 +11,5 @@ mariadb -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 mariadb -e "FLUSH PRIVILEGES;"		# Apply changes
+echo "Database initialized."
 service mysql stop					# Getting ready for next step
