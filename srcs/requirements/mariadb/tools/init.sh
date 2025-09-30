2ca17a1bc7359df0578c5c246076873685ef90de
Veliga Syutkin <veliga99@gmail.com>
2025-09-30 17:48:41 +0200

    fix: no temporary launch of db during init-db

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,9 +2,6 @@
 # service mysql start
 echo "Initializing database..."
 
-mysqld_safe --skip-networking &
-sleep 5
-
 mariadb -h localhost -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -h localhost -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
 mariadb -h localhost -e "DROP USER IF EXISTS '$MYSQL_USER'@'localhost';"
@@ -15,4 +12,3 @@ mariadb -h localhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_US
 mariadb -h localhost -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 mariadb -h localhost -e "FLUSH PRIVILEGES;"		# Apply changes
 echo "Database initialized."
-kill %1					# Getting ready for next step
