ab6d7a21f7ac5483d9cbfe2ef372dcd7a76bd6bb
Veliga Syutkin <veliga99@gmail.com>
2025-09-30 17:01:12 +0200

    fix: mariadb init script

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-service mysql start
+# service mysql start
 echo "Initializing database..."
+
+mysqld_safe --skip-networking &
 sleep 5
+
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
 mariadb -e "DROP USER IF EXISTS '$MYSQL_USER'@'localhost';"
@@ -12,4 +15,4 @@ mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
 mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 mariadb -e "FLUSH PRIVILEGES;"		# Apply changes
 echo "Database initialized."
-service mysql stop					# Getting ready for next step
+kill %1					# Getting ready for next step
