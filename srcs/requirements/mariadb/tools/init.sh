2aef3e8dfb8dd8c827856a9f3804d18293829247
Veliga Syutkin <veliga99@gmail.com>
2025-09-30 16:43:39 +0200

    db without acess wtf

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,9 +3,11 @@ service mysql start
 sleep 5
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "DROP USER IF EXISTS '$MYSQL_USER'@'%';"
+mariadb -e "DROP USER IF EXISTS '$MYSQL_USER'@'localhost';"
 mariadb -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
 mariadb -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
 mariadb -e "FLUSH PRIVILEGES;"		# Apply changes
 service mysql stop					# Getting ready for next step
